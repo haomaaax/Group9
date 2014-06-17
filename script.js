b9440fb11f63433a4ca3c20b64ed75c70dcc0f3a
msyanzi <maxyay5566@gmail.com>
2014-06-17 16:04:09 +0800

    f50

--- script.js
+++ script.js
@@ -256,7 +256,7 @@ function deleteAllCookies() {
 				  	for(var i=0; i<obj.length; i=i+1){
 				  		//console.log( (parseInt(obj[i],10)) );
 
-                        if(obj[i] <= (parseInt(Year+Month+Day) - 7) ){
+                        if(parseInt(obj[i]) <= (parseInt(Year+Month+Day) - 7) ){
                             $('#refrigerator').append('<div class="box_red" value='+obj[i-1]+'>'+obj[i-1]+"</div>");
                             console.log("FUCK YEAH!")
                         }
